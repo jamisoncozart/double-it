cc5405168a6bbb39d8b9cb22be841ea22e2beb13
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-21 11:49:48 -0800

    Fix variable types, add type conversions

--- doubleIt.cs
+++ doubleIt.cs
@@ -5,8 +5,10 @@ class DoubleIt
   static void Main()
   {
     Console.WriteLine("Give me a number and I will double it for you!");
-    int yourNumber = Console.ReadLine();
+    string stringNumber = Console.ReadLine();
+    int yourNumber = int.Parse(stringNumber);
     int yourDoubledNumber = yourNumber * 2;
-    Console.WriteLine("Your doubled number is " + yourDoubledNumber);
+    string doubledNumberString = yourDoubledNumber.ToString();
+    Console.WriteLine("Your doubled number is " + doubledNumberString);
   }
 }
