c6f947a55f9df79c5c8898a4916609987d07dd1c
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-21 11:47:03 -0800

    Double user input, print string with doubledNumber

--- doubleIt.cs
+++ doubleIt.cs
@@ -6,5 +6,7 @@ class DoubleIt
   {
     Console.WriteLine("Give me a number and I will double it for you!");
     int yourNumber = Console.ReadLine();
+    int yourDoubledNumber = yourNumber * 2;
+    Console.WriteLine("Your doubled number is " + yourDoubledNumber);
   }
 }
